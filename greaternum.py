0942bef40789ca88927831ff1875952025c9aaf2
567yash <yashabburi2005@gmail.com>
2026-09-08 19:27:25 +0530

    added python practice files

---
+++ greaternum.py
@@ -0,0 +1,20 @@
+
+#Method 1
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+# if n1>n2>n3:
+#     print(n1, "is the greatest number")
+# elif n2>n1>n3:
+#     print(n2,"is the greatest number")
+# else:
+#     print(n3," is the greatest number")
+
+
+#Method 2
+if n1>n2 and n1>n3 :
+    print(n1, "=n1 is greatest number" )
+elif n2>n1 and n2>n3 :
+    print (n2,"=n2 is greatest number")
+else:
+    print(n3,"=n3 is biggest number")
---
+++ isalpha.py
@@ -0,0 +1,7 @@
+n = input()
+if n.isalpha():
+    print(n,"is alphabet")
+elif n.isdigit():
+    print(n,"is digit")
+else:
+    print(n,"is special character")
